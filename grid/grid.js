5313a21e29dbdadb0bb8fd2ae22be486c3f76971
Jae-yi Ko <jaeko225540@gmail.com>
2021-04-28 12:37:00 -0700

    added comments

--- grid/grid.js
+++ grid/grid.js
@@ -6,18 +6,18 @@ var color=["blue","green","red", "yellow"]
 var nextcolor=0;
 
 function changeColor(){
-    var outerdiv=document.getElementById("grid")
-    var boxes= outerdiv.getElementsByTagName("div")
+    var outerdiv=document.getElementById("grid")//all the divs within id=grid are in outerdiv
+    var boxes= outerdiv.getElementsByTagName("div")//all of the divs 
 
     for(i=0; i<boxes.length; i++)
     {
         boxes[i].style.backgroundColor=color[nextcolor];
     }
-        if (nextcolor<color.length-1)
+        if (nextcolor<color.length-1)//if nextcolor's index is less than index 3=yellow then with each click the color will change to the next index
         {
-            nextcolor +=1
+            nextcolor +=1//each index will increment which means with each click there will be a color change in the circles
         }
         else{
-            nextcolor=0
+            nextcolor=0//if array color index is 3=3(aka yellow) then it will go back to blue
         }    
 }
